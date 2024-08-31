dd9f32be58f8233fddfd7b2700beb8475fa2a6f0
Aditya Suryadi <adityasuryaditya@gmail.com>
2024-08-31 07:19:00 +0700

    remove unused file

--- app/Repositories/Impl/EmployeeRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories;
-
-interface EmployeeRepository
-{
-    public function Insert(array $payload);
-    public function GetEmployeeByDepartement(int $departementId);
-}
